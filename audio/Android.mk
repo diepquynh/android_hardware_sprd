9b30597a4a0740f4752972faf9ddc3f88ca17d63
Quyen <quyenach1234@gmail.com>
2016-07-23 12:50:14 +0700

    sprd-common: audio: Add sc8830 source

--- audio/Android.mk
+++ audio/Android.mk
@@ -22,7 +22,7 @@ supported_boards := \
 ifneq (,$(filter $(supported_boards),$(TARGET_BOARD_PLATFORM)))
 
 ifneq (,$(filter sc8830 scx15,$(TARGET_BOARD_PLATFORM)))
-include $(call all-named-subdir-makefiles,sc8830)
+include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
 endif
 
 endif
