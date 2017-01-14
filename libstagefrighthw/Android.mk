1869397b526f5f279ca99b3d6ae0176e5cb6e0e4
Remilia Scarlet <remilia.1505@gmail.com>
2017-01-14 14:33:10 +0700

    libstagefrighthw: Import sc8830 code from SPRD vendor
    Also move the current sc8830 codes to scx15
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -15,16 +15,12 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
 supported_boards := \
 	sc8830 \
 	scx15 \
 
-ifneq (,$(filter $(supported_boards),$(TARGET_BOARD_PLATFORM)))
-
-ifneq (,$(filter scx15 sc8830,$(TARGET_BOARD_PLATFORM)))
-include $(call all-subdir-makefiles,sc8830)
-endif
-
+ifeq ($(SOC_SCX30G_V2),true)
+include $(call all-named-subdir-makefiles,sc8830)
+else ifneq (,$(filter $(supported_boards),$(TARGET_BOARD_PLATFORM)))
+include $(call all-named-subdir-makefiles,scx15)
 endif
