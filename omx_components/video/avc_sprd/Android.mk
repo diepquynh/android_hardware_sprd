a0c7a0294942317643074091d65c64f5bed2c2d6
Remilia Scarlet <remilia.1505@gmail.com>
2017-01-14 06:08:36 -0500

    OMX: scx30g_v2: Fix derp
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- omx_components/video/avc_sprd/Android.mk
+++ omx_components/video/avc_sprd/Android.mk
@@ -26,7 +26,7 @@ include_makefiles := \
 
 ifeq (sc8830,$(TARGET_BOARD_PLATFORM))
 ifeq ($(SOC_SCX30G_V2),true) # For scx30g_v2 board (sc8830)
-include_makefiles += $(call all-named-subdir-makefiles,scx30g_v2 sc8830/enc)
+include_makefiles += $(call all-named-subdir-makefiles,scx30g_v2)
 else
 include_makefiles += $(call all-named-subdir-makefiles,sc8830)
 endif
