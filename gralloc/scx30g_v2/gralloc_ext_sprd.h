4b346596225eb3b48a8184f28f2888d76d3d155b
Remilia Scarlet <remilia.1505@gmail.com>
2017-01-14 16:23:06 +0700

    gralloc: scx30g_v2: Nuke ion_invalidate_fd()
    We don't have ION_SPRD_CUSTOM_INVALIDATE in our kernel headers, so let's use
    ion_invalidate_fd() from libion_sprd instead
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- gralloc/scx30g_v2/gralloc_ext_sprd.h
+++ gralloc/scx30g_v2/gralloc_ext_sprd.h
@@ -27,20 +27,4 @@ enum
 	GRALLOC_USAGE_VIDEO_BUFFER		=  0x05000000,
 	GRALLOC_USAGE_CAMERA_BUFFER 		=  0x05000000,
 };
-#ifdef __cplusplus
-extern "C" {
-#endif
-static inline int ion_invalidate_fd(int fd, int handle_fd)
-{
-	struct ion_custom_data custom_data;
-	if (handle_fd < 0)
-		return -EINVAL;
-	custom_data.cmd = ION_SPRD_CUSTOM_INVALIDATE;
-	custom_data.arg = (unsigned long)handle_fd;
-	return ioctl(fd, ION_IOC_CUSTOM, &custom_data);
-}
-
-#ifdef __cplusplus
-}
-#endif
 #endif // GRALLOC_PRIV_EXT_H_
