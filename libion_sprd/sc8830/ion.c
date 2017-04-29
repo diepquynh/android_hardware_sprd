740da883f595357ca5b1025ae9d0f6ceffebf017
Remilia Scarlet <remilia.1505@gmail.com>
2017-04-29 15:29:13 +0700

    Revert "libion_sprd: sc8830: Bring back ion_invalidate_fd() from scx15"
    This reverts commit 47ca5a190b234ad5478824f2f9af806c847f7a51.
    
    https://github.com/remilia15/android_hardware_sprd/commit/4012ff16dbba2d7defd7e0fb938faa009032011d
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- libion_sprd/sc8830/ion.c
+++ libion_sprd/sc8830/ion.c
@@ -169,14 +169,6 @@ int ion_import(int fd, int share_fd, ion_user_handle_t *handle)
     return ret;
 }
 
-int ion_invalidate_fd(int fd, int handle_fd)
-{
-    struct ion_fd_data data = {
-        .fd = handle_fd,
-    };
-    return ion_ioctl(fd, ION_IOC_INVALIDATE, &data);
-}
-
 int ion_sync_fd(int fd, int handle_fd)
 {
     struct ion_fd_data data = {
