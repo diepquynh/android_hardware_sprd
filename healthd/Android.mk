fa967862ba5aa50bc53e2317f9896a14ad0ed22b
Remilia Scarlet <remilia.1505@gmail.com>
2017-10-31 14:57:40 +0000

    healthd: Fix compile
    https://github.com/LineageOS/android_vendor_lineage/commit/f69910d67fa2a8844d6cde7623d4273d36c77f23
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -20,5 +20,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd.cpp
 LOCAL_MODULE := libhealthd.$(TARGET_BOARD_PLATFORM)
-LOCAL_C_INCLUDES := system/core/healthd/include
+LOCAL_C_INCLUDES := \
+	system/core/healthd/include \
+	system/core/base/include \
+	bootable/recovery/minui/include
+
 include $(BUILD_STATIC_LIBRARY)
