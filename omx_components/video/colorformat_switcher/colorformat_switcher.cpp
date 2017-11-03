70be194f5426f34401af0c18e714154153a903a6
Remilia Scarlet <remilia.1505@gmail.com>
2017-11-03 05:08:31 +0000

    OMX: Don't switch colorformat on TARGET_UPDATED_MALI targets
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- omx_components/video/colorformat_switcher/colorformat_switcher.cpp
+++ omx_components/video/colorformat_switcher/colorformat_switcher.cpp
@@ -21,6 +21,7 @@
 
 void setColorFormat(OMX_COLOR_FORMATTYPE &eNativeColorFormat)
 {
+#ifndef UPDATED_MALI
     switch (eNativeColorFormat) {
         case OMX_COLOR_FormatYUV420SemiPlanar:
             eNativeColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_YCbCr_420_SP;
@@ -30,4 +31,5 @@ void setColorFormat(OMX_COLOR_FORMATTYPE &eNativeColorFormat)
             eNativeColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_YCbCr_420_P;
             break;
     }
+#endif
 }
