1a4f6ebe2caf9df67ffd4990d789475d668fa923
Dominggoes Isakh <drjisakh@gmail.com>
2017-12-21 23:58:21 +0100

    libril: Handle legacy radiostate-change for old RIL's
    Change-Id: I8177943b2d2adb866107184d24a40bceaf89f92b

--- ril/libril/ril_service.h
+++ ril/libril/ril_service.h
@@ -711,6 +711,7 @@ int sendRequestStringsResponse(int slotId,
 
 pthread_rwlock_t * getRadioServiceRwlock(int slotId);
 
+int processRadioState(int newRadioState, int slotId, int indicationType, int token, RIL_Errno e);
 }   // namespace radio
 
 #endif  // RIL_SERVICE_H
