8e5a8e04d24592049a3bc95b8e16fdd9a05677e8
Remilia Scarlet <remilia.1505@gmail.com>
2018-06-21 17:23:14 +0000

    gralloc: Ditch scx30g2 gralloc source
    It's buggy for now
    
    Change-Id: I8f3664efb2028f42c48e02007c10545c5bd0c399
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -21,13 +21,6 @@ supported_boards := \
 	sc8830 \
 	scx15 \
 
-ifeq ($(SOC_SCX30G_V2),true)
-
-ifeq ($(TARGET_UPDATED_MALI),true)
-include $(call all-named-subdir-makefiles,scx30g2)
-endif
-
-else ifneq (,$(filter $(supported_boards),$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter $(supported_boards),$(TARGET_BOARD_PLATFORM)))
 include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
-
 endif
