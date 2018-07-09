7488a961e052edc9d5f33bae9d14ec73dc9b078a
Quang Ngô <ngoquang2708@gmail.com>
2018-07-09 22:46:06 +0700

    ril: ignore invalid CommandInterface pointer value
    reference-ril some how send some mysterious response that has invalid
    pointer value of CommandInterface. Guessing the invalid value range
    is from 0 to 0xff and ignore it.
    Radio log at that point:
    ```
    07-09 22:49:20.516   167   336 V RILClient: [*] RecordReqHistory(): token(1), ID(59)
    07-09 22:49:20.516   167   336 V RILClient: [*] RecordReqHistory(): token(1), ID(59)
    07-09 22:49:20.516   167   336 V RILClient: SendOemRequestHookRaw(): token = 1
    07-09 22:49:20.516   167   336 V RILClient: [*] FindReqHandler(): token(1)
    07-09 22:49:20.517   167   336 V RILClient: [*] ClearReqHistory(): token(1)
    07-09 22:49:20.524   191   391 E secril-shim: pRI->pCI is invalid: 14
    07-09 22:49:20.524   191   391 E RILC    : RIL_onRequestComplete: invalid RIL_Token
    07-09 22:49:20.525   167  1702 V RILClient: processRxBuffer: status 0 response_type 0
    07-09 22:49:20.525   167  1702 V RILClient: processSolicited()
    07-09 22:49:20.525   167  1702 E RILClient: processSolicited: Invalid Token
    07-09 22:49:20.525   167  1702 E RILClient: RxReaderFunc: processRXBuffer returns 3
    07-09 22:49:20.525   167  1702 V RILClient: [*] RxReaderFunc() b_connect=1
    ```
    
    Change-Id: Iaba39e3be95b1f607244a063d697ffe7d6ba7e96

--- ril/libsecril-shim/secril-shim.cpp
+++ ril/libsecril-shim/secril-shim.cpp
@@ -249,9 +249,9 @@ static void onRequestCompleteShim(RIL_Token t, RIL_Errno e, void *response, size
 		goto null_token_exit;
 	}
 
-	/* If pCI is null, this entire function is useless. */
-	if (pRI->pCI == NULL) {
-		RLOGE("pRI->pCI is NULL");
+	/* If pCI is null or invalid pointer, this entire function is useless. */
+	if (!pRI->pCI || (0 <(int)pRI->pCI && (int)pRI->pCI < 0xff)) {
+		RLOGE("Invalid CommandInterface pointer: %p", pRI->pCI);
 		goto null_token_exit;
 	}
 
