01285620935cecfd483b6b789898f2fd790d1d30
Remilia Scarlet <remilia.1505@gmail.com>
2018-07-11 17:21:23 +0000

    ril: Import modemd from sprd vendor
    Change-Id: Ib2ab0c3d0bbd2633032ee1c32995e25098e7c382
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

---
+++ ril/modemd/sc8830/Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    modemd.c      \
+    modemd_vlx.c  \
+    modemd_ext.c  \
+    modemd_sipc.c
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libhardware_legacy \
+    liblog
+
+ifeq ($(strip $(TARGET_USERIMAGES_USE_EXT4)),true)
+LOCAL_CFLAGS := -DCONFIG_EMMC
+endif
+
+ifeq ($(BOARD_SECURE_BOOT_ENABLE), true)
+LOCAL_CFLAGS += -DSECURE_BOOT_ENABLE
+endif
+
+ifeq ($(SOC_SCX30G_V2),true)
+LOCAL_CFLAGS += -DSCX30G_V2
+endif
+
+LOCAL_MODULE := modemd
+
+LOCAL_PROPRIETARY_MODULE := true
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
