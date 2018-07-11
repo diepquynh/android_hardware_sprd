7a0e17c2682d6295290247730dda344ee44fe512
Remilia Scarlet <remilia.1505@gmail.com>
2018-07-11 10:05:20 +0000

    libsecril-client: Adapt for sprd
    Change-Id: I071f4018268b4d7d7bae407d425143baf975ee45
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- ril/libsecril-client/Android.mk
+++ ril/libsecril-client/Android.mk
@@ -1,7 +1,9 @@
 # Copyright 2006 The Android Open Source Project
 
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE_TAGS := eng
 
 LOCAL_SRC_FILES:= \
@@ -16,14 +18,19 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_CFLAGS := 
 
-ifeq ($(TARGET_BOARD_PLATFORM),exynos4)
+ifeq ($(TARGET_BOARD_PLATFORM),sc8830 scx15)
 LOCAL_CFLAGS += -DRIL_CALL_AUDIO_PATH_EXTRAVOLUME
 endif
+
 ifneq ($(filter m7450 mdm9x35 ss333 xmm7260,$(BOARD_MODEM_TYPE)),)
 LOCAL_CFLAGS += -DSAMSUNG_NEXT_GEN_MODEM
 endif
 
-LOCAL_MODULE:= libsecril-client
+LOCAL_MODULE:= libsecril-client-sprd
+
+LOCAL_POST_INSTALL_CMD := \
+	mv $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/$(LOCAL_MODULE).so $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libsecril-client.so
+
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
