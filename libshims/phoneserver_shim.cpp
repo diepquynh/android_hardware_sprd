32e3dd88f4018f6bb2c994e63c4fb247591f2c73
Quang Ngô <ngoquang2708@gmail.com>
2018-07-16 22:25:01 +0700

    shim: add shim for phoneserver
        pthread's pthread_setschedparam() and pthread_getschedparam()
    expected the first param which is pthread_t to be passed as value. But
    some how the phoneserver binary passed pthread_t as a pointer. The old
    behavior of bionic pthread is just ignore that invalid pthread_t and
    return null. New behavior of bionic pthread is changed in O and cause
    phoneserver to crash.
    
        This shim treads the pthread_t passed to pthread_setschedparam()
    and pthread_getschedparam() as a pointer an passed the deferenced pointer
    back to the original function.
    
    Change-Id: I8d792675f1d78229e633e82e2f9e802f96f8639d

---
+++ libshims/phoneserver_shim.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dlfcn.h>
+#include <pthread.h>
+
+#define LOG_TAG "PHONESERVER_SHIM"
+#define LOG_NDEBUG 0
+#include <cutils/log.h>
+
+extern "C" int pthread_setschedparam(pthread_t thread, int policy, const struct sched_param *param)
+{
+    ALOGI("%s: pointer=%p value=%lx", __FUNCTION__, (pthread_t*)thread, *((pthread_t*)thread));
+    return ((int (*)(pthread_t, int, const struct sched_param*)) dlsym(RTLD_NEXT, "pthread_setschedparam"))(*((pthread_t *)thread), policy, param);
+}
+
+extern "C" int pthread_getschedparam(pthread_t thread, int* policy, struct sched_param* param)
+{
+    ALOGI("%s: pointer=%p value=%lx", __FUNCTION__, (pthread_t *)thread, *((pthread_t *)thread));
+    return ((int (*)(pthread_t, int*, struct sched_param*)) dlsym(RTLD_NEXT, "pthread_getschedparam"))(*((pthread_t *)thread), policy, param);
+}
+
+extern "C" int pthread_create(pthread_t* thread, const pthread_attr_t* attr,void *(*start_rountine)(void*), void* arg)
+{
+    ALOGI("%s: pointer=%p, value=%lx", __FUNCTION__, thread, *thread);
+    return ((int (*)(pthread_t*, const pthread_attr_t*,void *(*)(void*), void*)) dlsym(RTLD_NEXT, "pthread_create"))(thread, attr, start_rountine, arg);
+}
